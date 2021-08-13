22c70c5ffe025b78410915c72f69970f8da8b3ca
djay.yim <angelsocer@gmail.com>
2021-08-14 00:03:33 +0900

    first commit

---
+++ src/main/java/google/djay/djayrestapistudy01/DjayRestApiStudy01Application.java
@@ -0,0 +1,13 @@
+package google.djay.djayrestapistudy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjayRestApiStudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjayRestApiStudy01Application.class, args);
+    }
+
+}
---
+++ src/test/java/google/djay/djayrestapistudy01/DjayRestApiStudy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package google.djay.djayrestapistudy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjayRestApiStudy01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
